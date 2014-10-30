44e90c888d6024922ba3b1ffd04d87d860717ff0
Neminath <ndpatil1008@gmail.com>
2014-10-30 20:15:26 +0530

    Add user microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,8 @@
 class MicropostsController < ApplicationController
-  before_action :set_micropost, only: [:show, :edit, :update, :destroy]
+  #before_action :set_micropost, only: [:show, :edit, :update, :destroy]
+  before_action :signed_in_user, only: [:create, :destroy]
+  before_action :correct_user, only: :destroy
+
 
   # GET /microposts
   # GET /microposts.json
@@ -24,17 +27,27 @@ class MicropostsController < ApplicationController
   # POST /microposts
   # POST /microposts.json
   def create
-    @micropost = Micropost.new(micropost_params)
+    #@micropost = Micropost.new(micropost_params)
 
-    respond_to do |format|
-      if @micropost.save
-        format.html { redirect_to @micropost, notice: 'Micropost was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @micropost }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @micropost.errors, status: :unprocessable_entity }
-      end
-    end
+    #respond_to do |format|
+      #if @micropost.save
+       # format.html { redirect_to @micropost, notice: 'Micropost was successfully created.' }
+        #format.json { render action: 'show', status: :created, location: @micropost }
+      #else
+        #format.html { render action: 'new' }
+        #format.json { render json: @micropost.errors, status: :unprocessable_entity }
+      #end
+    #end
+  @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+    flash[:success] = "Micropost created!"
+    redirect_to root_url
+   else
+   @feed_items = []
+  render 'static_pages/home'
+
+end
+
   end
 
   # PATCH/PUT /microposts/1
@@ -54,11 +67,13 @@ class MicropostsController < ApplicationController
   # DELETE /microposts/1
   # DELETE /microposts/1.json
   def destroy
+   # @micropost.destroy
+    #respond_to do |format|
+     # format.html { redirect_to microposts_url }
+      #format.json { head :no_content }
+    #end
     @micropost.destroy
-    respond_to do |format|
-      format.html { redirect_to microposts_url }
-      format.json { head :no_content }
-    end
+   redirect_to root_url
   end
 
   private
@@ -69,6 +84,15 @@ class MicropostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def micropost_params
-      params.require(:micropost).permit(:content, :user_id)
+      params.require(:micropost).permit(:content)
     end
+ 
+   def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to root_url if @micropost.nil?
+   
+   end
+
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 before_save { self.email = email.downcase }
 before_create :create_remember_token
+has_many :microposts, dependent: :destroy
 
 VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 validates :name, presence:true 
@@ -15,6 +16,12 @@ end
 
 def User.digest(token)
 Digest::SHA1.hexdigest(token.to_s)
+end
+
+
+def feed
+# This is preliminary. See "Following users" for the full implementation.
+Micropost.where("user_id = ?", id)
 end
 
 
